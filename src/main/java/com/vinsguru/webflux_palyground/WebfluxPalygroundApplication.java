a833f6bf5c38b2ba68648d0ef48e80466ead7815
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-09-15 20:28:00 +0545

    spring webflux started

---
+++ src/main/java/com/vinsguru/webflux_palyground/WebfluxPalygroundApplication.java
@@ -0,0 +1,13 @@
+package com.vinsguru.webflux_palyground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPalygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPalygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinsguru/webflux_palyground/WebfluxPalygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinsguru.webflux_palyground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPalygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
